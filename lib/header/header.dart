4bcd4c9b99e797e9be70989c35b14bfbc54c1302
Kim GyeongMin <gyeongmin951@naver.com>
2023-05-06 22:33:49 +0900

    [ Add ] 알람 등록 헤더

--- lib/header/header.dart
+++ lib/header/header.dart
@@ -61,9 +61,87 @@ class _KeepGoingHeaderState extends State<KeepGoingHeader> {
                       icon: const Icon(Icons.add),
                       onPressed: () {
                         Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => const App()),
+                          MaterialPageRoute(builder: (context) => const AlarmFormPage(title: 'title')),
                           // MaterialPageRoute(builder: (context) => const LocationBasedAlarmWidget()),
                         );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            );
+  }
+}
+
+
+
+
+class KeepGoingFormHeader extends StatefulWidget {
+  const KeepGoingFormHeader({super.key});
+
+  @override
+  State<KeepGoingFormHeader> createState() => _KeepGoingFormHeaderState();
+}
+
+class _KeepGoingFormHeaderState extends State<KeepGoingFormHeader> {
+  void onClicked() {
+    setState(() {
+    
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: IconButton(
+                      icon: const Text(
+                        '취소',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Colors.red
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: const Text(
+                      '알람 추가',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: IconButton(
+                      icon: const Text(
+                        '등록',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Colors.green
+                        ),
+                      ),
+                      onPressed: () {
+                        // TODO 알람 등록 유효성 검사
                       },
                     ),
                   ),
